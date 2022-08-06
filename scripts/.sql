01deb8dd424defd70be06a913b3910464f8683c5
Hannah Solima <hannahsolima@gmail.com>
2022-08-06 11:46:03 -0500

    bonuns q

---
+++ scripts/.sql
@@ -0,0 +1,92 @@
+---Question 1
+SELECT COUNT (*) 
+FROM data_analyst_jobs; 
+---ANSWER:1793
+
+---Question 2
+SELECT *
+FROM data_analyst_jobs
+LIMIT 10;
+---ANSWER: ExxonMobil
+
+---Question 3
+SELECT *
+FROM data_analyst_jobs
+WHERE location = 'TN';
+---ANSWER: 21
+
+---Question 3 (continued)
+SELECT *
+FROM data_analyst_jobs
+WHERE location = 'TN' OR location = 'KY';
+---ANSWER: 27
+
+---Question 4
+SELECT *
+FROM data_analyst_jobs
+WHERE star_rating >4
+AND location = 'TN';
+---ANSWER: 3
+
+---Question 5
+SELECT *
+FROM data_analyst_jobs
+WHERE review_count BETWEEN 500 AND 1000;
+---ANSWER: 151
+
+---Question 6
+SELECT location AS state, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+GROUP BY state
+ORDER BY avg_rating DESC;
+---ANSWER: NE (Nebraska?)
+
+---Question 7
+SELECT DISTINCT(title)
+FROM data_analyst_jobs;
+---ANSWER: 881
+
+---Question 8
+SELECT DISTINCT(title), location
+FROM data_analyst_jobs
+WHERE location = 'CA';
+---ANSWER: 230
+
+---Question 9
+SELECT company, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE review_count >5000 
+AND company IS NOT NULL
+GROUP BY company;
+---ANSWER:40
+
+---Question 10
+SELECT company, AVG(star_rating) AS avg_rating
+FROM data_analyst_jobs
+WHERE review_count >5000
+GROUP BY company
+ORDER BY avg_rating DESC, company;
+---ANSWER: American Express, avg_rating = 4.1999998...there are 5 other companies with the same rating, but I selected the first alphabetically
+
+---QUESTION 11
+SELECT DISTINCT(title)
+FROM data_analyst_jobs
+WHERE UPPER(title) LIKE '%ANALYST%';
+---ANSWER: 774, using COUNT - ANSWER: 1669
+
+---Question 12
+SELECT DISTINCT(title)
+FROM data_analyst_jobs
+WHERE UPPER(title) NOT LIKE '%ANALYST%' AND UPPER(title) NOT LIKE '%ANALYTICS%';
+---ANSWER: 4
+
+---BONUS
+SELECT *
+FROM data_analyst_jobs
+WHERE skill = 'SQL'
+AND days_since_posting >(3*7)
+AND domain IS NOT NULL
+ORDER BY 
+
+
+
